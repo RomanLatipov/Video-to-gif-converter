4abe2b4586385b376a92cce2d6b1e7dc18669b7e
RomanLatipov <romanlatipov@gmail.com>
2024-09-13 20:25:28 -0400

    configure ffmpeg and load video

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 import './App.css'
 
 function App() {
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
 
-  return (
-    <>
-    </>
-  )
+  const ffmpeg = createFFmpeg({ log: true });
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+
+  return (<>
+    {ready ?
+    <div className='app'>
+      <input type='file' onChange={(event) => setVideo(event.target.files?.item(0))}></input>
+    </div>
+    
+    : <p>Loading...</p>}
+  </>)
 }
 
 export default App
